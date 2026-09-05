813d624139287d70245394e99d9305a5cea14a74
MohamadhuAshik <mohamadhuashik@gmail.com>
2026-09-05 15:57:17 +0530

    Navigation-error fixing

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -2,18 +2,18 @@ import Navbar from "@/Components/Navbar";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-import { useRouter } from "next/router";
+
 
 export default async function PrivateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter()
+  // const router = useRouter()
    const { isAuthenticated } = await auth();
 
   if (!isAuthenticated) {
-    router.push("/login");
+    redirect("/login");
   }
 
   return (
