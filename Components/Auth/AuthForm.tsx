a35f5a7b35cb4cef1643e4d20b0582a570592a7d
MohamadhuAshik <mohamadhuashik@gmail.com>
2026-09-05 15:48:55 +0530

    Completed Clert auth

--- Components/Auth/AuthForm.tsx
+++ Components/Auth/AuthForm.tsx
@@ -1,5 +1,8 @@
 'use client'
-import React from 'react'
+import { SignIn, SignUp, useUser } from '@clerk/nextjs';
+
+import { useRouter } from 'next/navigation';
+
 
 
 type AuthFormProps = {
@@ -7,10 +10,28 @@ type AuthFormProps = {
 };
 
 const AuthForm = ({ mode }: AuthFormProps)=> {
+  const router = useRouter()
+  const isRegister = mode==="Register"
+  const {isLoaded,isSignedIn} = useUser()
+
+  // if(isLoaded && isSignedIn) {
+  //   return router.push("/dashboard")
+  // }
   return (
    <div className="min-h-screen w-full bg-[url('/login_bg.png')] bg-cover bg-no-repeat text-slate-800 p-4 md:p-6 lg:p-8 flex items-center justify-center font-sans ">
       <div className='w-full flex justify-center py-2'>
-      {mode}
+      {
+        isRegister ? (
+        <>
+         <SignUp routing='path' path='/register' signInUrl='/login ' fallbackRedirectUrl= "/dashboard" />
+        </>
+      ):
+      (
+      <>
+         <SignIn routing='path' path='/login' signUpUrl='/register ' fallbackRedirectUrl= "/dashboard" />
+      </>
+    )
+      }
     </div>
     </div>
   )
--- app/(public)/login/[[...login]]/page.tsx
+++ app/(public)/login/[[...login]]/page.tsx
@@ -1,11 +1,10 @@
 import AuthForm from '@/Components/Auth/AuthForm'
-import { SignIn } from "@clerk/nextjs";
+
 const page = () => {
   return (
-    <>
+    
        <AuthForm mode="Login"/>
-       <SignIn />
-    </>
+    
     
   )
 }
